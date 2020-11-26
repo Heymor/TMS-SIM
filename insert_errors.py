2f55c2688f140798233f1dcef3d7ef7f7b618cea
Heymor <11615676+Heymor@users.noreply.github.com>
2020-11-26 13:23:27 -0500

    Maxim error generation works!

--- insert_errors.py
+++ insert_errors.py
@@ -23,8 +23,8 @@ def float_to_maxim(f):
         sign_bit = 1
 
     temperature_bits = [0] * 32
-    temperature_bits[13] = sign_bit
-    j = 12
+    temperature_bits[31] = sign_bit
+    j = 30
     for p in reversed(range(-2, 11)):
         if (f - 2**(p)) >= 0:
             temperature_bits[j] = 1
@@ -33,24 +33,20 @@ def float_to_maxim(f):
             temperature_bits[j] = 0
         
         j -= 1
-    temperature_bin_str = ''.join(str(e) for e in temperature_bits)
+
+    temperature_bin_str = ''.join(str(e) for e in reversed(temperature_bits))
     ba = bytearray(int(temperature_bin_str, 2).to_bytes(len(temperature_bin_str) // 8, byteorder="big"))
     return ba
 
 def maxim_to_float(m):
-    print(binascii.hexlify(m))
-    print("0x%02x" % m[0])
     f = (m[0] & 0x7F) * 2.0**4
-    print(m[0])
     f += (m[1] & 0xFE) / 2.0**4
-    print(f)
     f *= (-1)**(m[0] >> 7)
-    print(f)
 
     return f
 
 def insert_error(data_point):
-    if random.random() > 0.5:
+    if random.random() > 0.75:
         # Flip random bit
         # First unpack the float
         data_point = float_to_bits(data_point)
@@ -68,18 +64,19 @@ def insert_error(data_point):
     return data_point
 
 def insert_maxim_error(data_point):
-    if random.random() > 0.5:
+    if random.random() > 0.75:
         # Flip a random bit
         # First translate from temperature point to maxim 
         data_point = float_to_maxim(data_point)
 
         # Get a random bit position
-        num_bits = data_point.bit_length()
+        num_bits = len(data_point) * 8
         rand_bit = random.randrange(num_bits)
+        rand_byte = rand_bit // 8
+        rand_bit_loc = rand_bit - (rand_byte * 8)
 
         # Flip bit
-        # This is assuming it's an int
-        data_point ^= 1 << rand_bit
+        data_point[rand_byte] ^= 1 << rand_bit_loc
 
         # Change back to float
         data_point = maxim_to_float(data_point)
@@ -125,9 +122,4 @@ def insert_maxim_temperature_errors(csv_filepath, csv_writeout_filepath):
     run_data_frame.to_csv(csv_writeout_filepath, sep=',')
 
 # TODO: This won't work... Need to first fix translation to MAXIM
-# insert_maxim_temperature_errors('./Data/long_ramp_hold_2020-11-13.csv', 'test.csv')
-a = float_to_maxim(133.75)
-a = maxim_to_float(a)
-print(a)
-
-# print(insert_maxim_error(133.75))
+insert_maxim_temperature_errors('./Data/long_ramp_hold_2020-11-13.csv', 'test.csv')
